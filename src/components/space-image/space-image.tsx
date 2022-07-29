ca7bb721dc8d7e7c4168d93b57c8ebe05352fe00
Elmira Ilyasova <capricorn_26@mail.ru>
2022-07-29 14:58:40 +0700

    Add space image

---
+++ src/components/space-image/space-image.tsx
@@ -0,0 +1,31 @@
+import * as React from "react";
+
+const SpaceImage = (): JSX.Element => {
+  return (
+    <div className="space-image">
+      <div className="space-image__paper-planes"></div>
+      <div className="space-image__girl">
+        <div className="space-image__pencil"></div>
+      </div>
+
+      <div className="space-image__space">
+        <div className="space-image__atom"></div>
+        <div className="space-image__moon"></div>
+        <div className="space-image__alarm"></div>
+        <div className="space-image__rocket"></div>
+        <div className="space-image__striped-planet"></div>
+        <div className="space-image__aerostat"></div>
+        <div className="space-image__earth"></div>
+        <div className="space-image__bulb"></div>
+        <div className="space-image__plus"></div>
+        <div className="space-image__division"></div>
+        <div className="space-image__elephant"></div>
+        <div className="space-image__palette"></div>
+        <div className="space-image__plane"></div>
+        <div className="space-image__stars"></div>
+      </div>
+    </div>
+  )
+};
+
+export default SpaceImage;
