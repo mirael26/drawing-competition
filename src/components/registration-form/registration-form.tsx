1e5c4874daa0723b3c20ae608353dae14a6acba8
Elmira Ilyasova <capricorn_26@mail.ru>
2022-08-01 12:43:13 +0700

    Add Registration form component

---
+++ src/components/registration-form/registration-form.tsx
@@ -0,0 +1,37 @@
+import * as React from "react";
+
+import TextInput from "../generic/text-input/text-input";
+import CheckboxAgreement from "../checkbox-agreement/checkbox-agreement";
+import Button from "../generic/button/button";
+
+const RegistrationForm = (): JSX.Element => {
+  return (
+    <form className="registration-form">
+      <h2 className="registration-form__title">Зарегистрируйтесь, чтобы участвовать</h2>
+
+      <div className="registration-form__inputs">
+        <TextInput placeholder={'ФИО ребенка'} />
+        <TextInput placeholder={'Дата рождения'} />
+        <TextInput placeholder={'Город'} />
+        <TextInput type={'email'} placeholder={'Email'} />
+        <TextInput type={'password'} placeholder={'Пароль'} />
+      </div>
+
+      <div className="registration-form__agreement">
+        <CheckboxAgreement mode={'dark'} checked={true} id={'registration-agreement'}/>
+      </div>
+
+      <div className="registration-form__submit-button">
+        <Button color={'additional-accent'} shadow text={'Участвовать'} />
+      </div>
+
+      <div className="registration-form__paper-planes">
+        <div className="registration-form__paper-plane-1"></div>
+        <div className="registration-form__paper-plane-2"></div>
+        <div className="registration-form__paper-plane-3"></div>
+      </div>
+    </form>
+  )
+};
+
+export default RegistrationForm;
--- src/components/registration/registration.tsx
+++ src/components/registration/registration.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 
 import RepostDiscount from "../repost-discount/repost-discount";
+import RegistrationForm from "../registration-form/registration-form";
 
 const Registration = (): JSX.Element => {
   return (
@@ -9,7 +10,9 @@ const Registration = (): JSX.Element => {
         <RepostDiscount />
       </div>
 
-      <div className="registration__registration"></div>
+      <div className="registration__registration-form">
+        <RegistrationForm />
+      </div>
     </div>
   )
 };
