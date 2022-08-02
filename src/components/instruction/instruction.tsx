7d2126e606c1772cba2be0e12635b13de38a0145
Elmira Ilyasova <capricorn_26@mail.ru>
2022-08-03 00:11:55 +0700

    Add animation to Instruction component

--- src/components/instruction/instruction.tsx
+++ src/components/instruction/instruction.tsx
@@ -1,25 +1,56 @@
 import * as React from "react";
+import { useState, useRef } from "react";
+
+import { offset } from "../../utils";
 
 const Instruction = (): JSX.Element => {
+  const [isVisible, setIsVisible] = useState(false);
+  const blockRef = useRef(null);
+
+  const checkVisibility = () => {
+    const block = blockRef.current;
+    const blockHeight = block.offsetHeight;
+    const blockOffsetY = offset(block).top;
+
+    window.innerHeight
+    window.pageYOffset
+
+    const visibilityPoint = (window.innerHeight > blockHeight) ? blockOffsetY - (window.innerHeight - blockHeight) : blockOffsetY;
+
+    if (window.pageYOffset > visibilityPoint) {
+      console.log('докрутили')
+      if (!isVisible) {
+        setIsVisible(true);
+      }
+      window.removeEventListener('scroll', checkVisibility);
+    }
+  }
+  
+  window.addEventListener('scroll', checkVisibility);
+
   return (
-    <div className="instruction">
+    <div className={`instruction${isVisible ? ' visible' : ''}`} ref={blockRef}>
+
       <h2 className="instruction__title">Пошаговый <span className="instruction__title instruction__title--accent">план</span></h2>
+
       <ol className="instruction__list">
-        <li className="instruction__item">
+        <li className="instruction__item instruction__item--1">
           <div className="instruction__number">1</div>
           <p className="instruction__text">Зарегистрируйтесь на конкурс <a className="instruction__link" href="#">Здесь</a></p>
         </li>
 
-        <li className="instruction__item">
+        <li className="instruction__item instruction__item--2">
           <div className="instruction__number">2</div>
           <p className="instruction__text">Выполните все условия конкурса</p>
         </li>
 
-        <li className="instruction__item">
+        <li className="instruction__item instruction__item--3">
           <div className="instruction__number">3</div>
           <p className="instruction__text">Загрузите рисунок <a className="instruction__link" href="#">Здесь</a> или в Личном кабинете</p>
         </li>
       </ol>
+
+      <div className="instruction__plane"></div>
     </div>
   )
 };
---
+++ src/utils.ts
@@ -0,0 +1,6 @@
+export const offset = (el: Element) => {
+  var rect = el.getBoundingClientRect(),
+  scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+  scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+  return { top: rect.top + scrollTop, left: rect.left + scrollLeft }
+}
